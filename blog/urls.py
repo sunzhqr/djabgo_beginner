16faa7ed5075a5d199ce3f463cca2d469f3103c3
sunzhqr <sanzhar_myrzash@mail.ru>
2024-05-18 13:29:27 +0500

    TEmplateView first try

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path
 from blog import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # path(route, view, kwargs=None, name=None)
     # re_path(route, view, kwargs=None, name=None)
     path('', views.index),
-    path('about/', views.about),
-    re_path(r'^contact/', views.contact),
+    path('about/', TemplateView.as_view(template_name="blog/about.html", extra_context={'header': 'About website'})),
+    re_path(r'^contact/', TemplateView.as_view(template_name="blog/contact.html")),
     path('details/', views.details),
     path('user/', views.user),
     path('access/<int:age>/', views.access),
